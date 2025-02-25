c3de31e39995fd3bb31fcd1cbac8e2f3ecb820a3
diegodelvalle3 <diegorogito@outlook.com>
2025-02-25 00:10:15 -0600

    nuevos componentes del dashboard

---
+++ src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -0,0 +1,31 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+interface MenuOption{
+  label: string,
+  sublabel: string,
+  route: string,
+  icon: string
+}
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [],
+  templateUrl: 'gifs-side-menu-options.component.html',
+})
+export class GifsSideMenuOptionsComponent {
+  MenuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      sublabel: 'Gifs Populares',
+      route: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      sublabel: 'Buscar Gifs',
+      route: '/dashboard/search',
+    }
+  ]
+
+ }
--- src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GifsSideMenuHeaderComponent } from '../../components/gifs-side-menu-header/gifs-side-menu-header.component';
+import { GifsSideMenuOptionsComponent } from '../../components/gifs-side-menu-options/gifs-side-menu-options.component';
+import { GifsSideMenuComponent } from '../../components/gifs-side-menu/gifs-side-menu.component';
 
 @Component({
   selector: 'selector-name',
   templateUrl: 'dashboard-page.component.html',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GifsSideMenuComponent],
   standalone: true
 })
 
